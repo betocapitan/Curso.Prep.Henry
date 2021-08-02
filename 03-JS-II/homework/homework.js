ba64b53410f05f026d9f5aea8a2c29ad25689b0e
betocapitan <largelescano@gmail.com>
2021-08-02 11:48:01 -0300

    hw 03-js-ii

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -148,8 +148,9 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
   var array = []
-  for (let i = 0; i <= 10; i++)
-  {array.push(i * 6);}return array;
+  for (let i = 0; i <= 10; i++){
+    array.push(i * 6);
+  }return array;
   
 }
 
